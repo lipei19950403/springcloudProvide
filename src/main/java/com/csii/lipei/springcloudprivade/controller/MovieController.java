5182fea4f4568041f91578ab353fc4c90f7fc79c
lipei19950403 <996891845@qq.com>
2019-01-08 18:12:26 +0800

    sadsad

--- src/main/java/com/csii/lipei/springcloudprivade/controller/MovieController.java
+++ src/main/java/com/csii/lipei/springcloudprivade/controller/MovieController.java
@@ -2,16 +2,27 @@ package com.csii.lipei.springcloudprivade.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.csii.lipei.springcloudprivade.entity.User;
+
 @RestController
 public class MovieController {
 	
+	//无参数接受请求
 	@RequestMapping("/SolrSearch")
-    public String SolrSearch(){
-        return "这里是Solr";
+    public User SolrSearch(){
+        return new User("lipei", "12345");
     }
+	
+	//有参数接受请求
+	@RequestMapping("/SolrSearchById/{id}")
+	public User SolrSearchById(@PathVariable(name = "id")String id) {
+		 return new User("lipei", id);
+	}
+	
 	
 }
